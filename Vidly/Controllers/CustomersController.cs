00d61a481247b01b0268d589f71a3b35cb3ec90d
Daniel Gonzalez <daniel.g287@yahoo.com>
2016-07-24 17:15:02 -0600

    Created Customer and Movies table and details view

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Daniel"},
+                new Customer { Id = 2, Name = "Erik"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id, string name)
+        {
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name
+            };
+
+            return View(customer);
+        }
+
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,10 +10,13 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        
         // GET: Movies/Random
+        
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Goodfellas" };
+
+            var movie = new Movie() { Name = "Interstellar" };
             var customers = new List<Customer>
             {
                 new Customer { Name = "Customer 1" },
@@ -28,5 +31,37 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
+
+
+
+        public ActionResult Display()
+        {
+            var movie = new List<Movie>
+            {
+                new Movie() { Id = 1, Name = "Interstellar" },
+                new Movie() { Id = 2, Name = "The Last Samurai" }
+            };
+
+            var viewMovies = new DisplayMoviesViewModel()
+            {
+                Movies = movie
+            };
+
+            return View(viewMovies);
+        }
+
+        [Route("movies/details/{id}")]
+        public ActionResult Details(int id, string name)
+        {
+
+            var movie = new Movie
+            {
+                Id = id,
+                Name = name
+            };
+
+            return View(movie);
+        }
+
     }
 }
